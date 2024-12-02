24c63c3ed5484540181c281c60837fbf4e4ad2b5
Ganesh16073 <ganeshkulkarni4237@gmail.com>
2024-12-02 12:16:13 +0530

    Learned Comparision

---
+++ Basic/04_Comparision.js
@@ -0,0 +1,23 @@
+console.log(2>1)
+console.log(3<1)
+console.log() // normal Conversion
+
+console.log("3"<1) // automaticaly type conversion
+console.log("9">8)
+
+console.log("")
+console.log(null>0) // sometimes NaN or 0 internal logic
+console.log(null>=0) // null was converted to 0
+console.log(null==0)
+console.log(null<0)
+
+console.log("")
+console.log(undefined>0)
+console.log(undefined>=0) 
+console.log(undefined==0)
+console.log(undefined<0)// every thing is false
+
+
+console.log("")
+console.log(20===20) // strite check
+console.log("20"===20) 
