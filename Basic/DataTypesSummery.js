f8c5b7294ba66d5139435a1fe583b332eaa39fd4
Ganesh16073 <ganeshkulkarni4237@gmail.com>
2024-12-02 13:50:54 +0530

    Deeply Learned DataTypes

---
+++ Basic/DataTypesSummery.js
@@ -0,0 +1,41 @@
+/*
+There are two types of Datatype in JS
+1) Premitive 
+2) NOn-Premitive/ Refrence Type
+
+======== Premitive Type ==========
+7 type 
+1) String
+2) Number
+3) Boolean
+4) null
+5) undifined
+6) Symbol  // const id=Symbol('123')
+7) BigInt
+
+======== Non- Premitive Type =======
+3 Types
+
+1) Array
+2) Object
+3)Functions
+
+arrray // let data=["apple","Bat"]
+
+Object 
+let myObj={
+name:"Ganesh",
+age:21
+}
+
+const myFuction=function()
+{
+consol.log("Hello Ajay")
+}
+
+Is JS is Static or Dynamic
+
+const=10 || true || "String" // it can store any type of data because it is Dynamic
+
+
+*/
