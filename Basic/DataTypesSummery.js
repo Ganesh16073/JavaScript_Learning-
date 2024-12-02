e38c59615f2991c381f4fc6fdfc15b07f08888ce
Ganesh16073 <ganeshkulkarni4237@gmail.com>
2024-12-02 14:12:27 +0530

    Learned Memeory allocation in JavaScript

--- Basic/DataTypesSummery.js
+++ Basic/DataTypesSummery.js
@@ -37,5 +37,37 @@ Is JS is Static or Dynamic
 
 const=10 || true || "String" // it can store any type of data because it is Dynamic
 
+======================================================================================================
+======================================================================================================
+
+>>>>>>>>>>>>>>>>>>>>>>>>>MEMORY ALLOCATION<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+There are TwoTypes Memory in JS
+1) Stack
+2) Heap
+
+Stack ->> Store Primitive type data
+Heap  ->> Store Non Primitive Type of Data
 
 */
+
+// Stack
+let myName="Ganesh"
+let anotherName=myName // does not point the address and it only retun th output
+anotherName="Gaurav"
+console.log(myName)
+console.log(anotherName)
+
+
+//Heap
+let user={
+    name:"Ganesh",
+    email:"ganeshkulkarni4237@gmail.com",
+    upi:"abc@ybi"
+}
+
+let userOne=user
+userOne.upi="ganesh@yml"
+
+console.log(user) //in both Object the value is changed because it shared refrence(address) Non-Premitive
+console.log(userOne)
