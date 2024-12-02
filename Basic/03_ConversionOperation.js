62e44380e57c43091dc3643e0aa8c99434ecf424
Ganesh16073 <ganeshkulkarni4237@gmail.com>
2024-12-02 12:03:17 +0530

    Learned Operation i javaScript

--- Basic/03_ConversionOperation.js
+++ Basic/03_ConversionOperation.js
@@ -60,4 +60,40 @@ console.log(someNumber) // 33 as number
 console.log(stringNumber) // 33 as string
 console.log(typeof stringNumber) // string
 
+/***********************OPERATIONS************************** */
+
+console.log()
+console.log(" ============================= OPERATION =============================")
+console.log()
+
+let value=3
+let negValue=-value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) // 2power3
+console.log(2/2)
+console.log(2%2)
+
+console.log("")
+let str="Good"
+let str2=" Evening"
+let str3=str+str2
+console.log(str3)
+
+console.log("1"+2) // string concate
+console.log(1+"5")
+console.log("1"+2+2) // left to right
+console.log(1+2+"2") // perform left to right opperation
+
+let num1,num2,num3;
+num1=num2=num3=99+1 // not recommended
+
+console.log("")
+let gameCounter=100
+console.log(gameCounter++)
+console.log(++gameCounter) // same as Java
+
 
