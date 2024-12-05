97f7e512aff0a23f340d078ed31602381eac4b3f
Ganesh16073 <ganeshkulkarni4237@gmail.com>
2024-12-05 10:06:38 +0530

    learnred function decleration type and their scope

--- 03_Basics/02_Scope.js
+++ 03_Basics/02_Scope.js
@@ -11,4 +11,26 @@ if(true)
 console.log(a);
 console.log(b);
 console.log(c);
+one() // we can call here also
+function one()
+{
+    const user="Ganesh"
+    function two()
+    {
+        const website="Spring"
+        console.log(user);
+    }
+    // console.log(website)
+    two()
+}
+
+
+// addFive(2) we cannot call here because it we stored function in variable  
+const addFive=function(num)
+{
+    return num+5
+}
+
+console.log(addFive(2));
+
 
