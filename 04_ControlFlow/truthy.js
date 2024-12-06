9dc82a6cd59e26211b41d90d43f9e7d17de0e3e5
Ganesh16073 <ganeshkulkarni4237@gmail.com>
2024-12-06 08:42:20 +0530

    worked on Nullish and Ternary operator

--- 04_ControlFlow/truthy.js
+++ 04_ControlFlow/truthy.js
@@ -7,3 +7,20 @@ if(useremail)
 else{
     console.log("Not got Email")
 }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let vall
+// vall=5 ?? 10
+console.log(vall);
+// vall=null?? 20 // if Null then next value is assingned
+console.log(vall);
+vall=undefined ?? 40 // if undefined then next Value
+console.log(vall);
+
+// Terniary Operator
+
+// Condition ? value : value
+
+const iceTeaPrice=100
+iceTeaPrice<=90 ? console.log("Average price ") : console.log("Expensive tea");
