168f6c1e615173840b71534ace98e8dc2e384da4
Ganesh_Kulkarni <ganeshkulkarni4237@gmail.com>
2024-12-20 14:12:43 +0530

    Developed a background changer

---
+++ 07_Project/06_UnlimiteCode/script.js
@@ -0,0 +1,36 @@
+// Generate Random Colour
+
+const RandomColour=function()
+{
+    const hex='0123456789ABCDEF'
+    let colour="#"
+    for(let i=0;i<6;i++)
+    {
+        colour+=hex[Math.floor(Math.random()*16)];
+    }
+    return colour;
+
+}
+
+let interval;
+
+let startChangingColour=function()
+{
+    let col=function()
+    {
+        document.body.style.backgroundColor=RandomColour()
+        console.log("Start");
+    }
+    interval=setInterval(col,1000)
+}
+
+let stopChangigTime=function()
+{
+    clearInterval(interval)
+    console.log("Stoped");
+    
+}
+
+document.querySelector("#start").addEventListener('click',startChangingColour)
+document.querySelector("#stop").addEventListener('click',stopChangigTime)
+
